7307088c665ced9a04ff62e6d49fe6df0cdc3324
Helmut Felix Essien <helmutessien@gmail.com>
2026-02-15 18:27:20 +0000

    Removed the blue section in the splash screen

--- MobileApp/Platforms/Android/MainActivity.cs
+++ MobileApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,33 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
 
 namespace MobileApp
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Set status bar color to match splash screen dark background (#1a1a1a)
+            if (Window != null)
+            {
+                Window.SetStatusBarColor(Android.Graphics.Color.Rgb(26, 26, 26));
+                
+                // Make status bar icons light colored for dark background
+                if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+                {
+                    var windowInsetsController = WindowCompat.GetInsetsController(Window, Window.DecorView);
+                    if (windowInsetsController != null)
+                    {
+                        windowInsetsController.AppearanceLightStatusBars = false;
+                    }
+                }
+            }
+        }
     }
 }
